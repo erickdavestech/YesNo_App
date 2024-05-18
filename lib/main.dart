d824738348150a12dde953117185c2db91b7c710
unknown <erickdavestech@gmail.com>
2024-05-18 11:06:39 -0400

    _01_debug_banner_remove

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MainApp());
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      // Elimamos el barnner de debug
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
